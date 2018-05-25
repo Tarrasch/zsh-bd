2991446078315737d4659807b4b200fd17b35c8f
Ryuto-MacBook <26628494+ryutok@users.noreply.github.com>
2018-05-25 18:56:58 +0900

    Except CWD from parents list

--- bd.zsh
+++ bd.zsh
@@ -12,25 +12,21 @@ bd () {
   # First try to find a folder with matching name (could potentially be a number)
   # Get parents (in reverse order)
   local parents
-  local i
-  for i in {$((num_folders_we_are_in+1))..2}
-  do
-    parents=($parents "$(echo $PWD | cut -d'/' -f$i)")
-  done
-  parents=($parents "/")
+  parents=(${(Oa)${(ps:/:)${PWD}}[1,-2]} "/")
   # Build dest and 'cd' to it
   local parent
   foreach parent (${parents})
   do
+    dest+="../"
     if [[ $1 == $parent ]]
     then
       cd $dest
       return 0
     fi
-    dest+="../"
   done
 
   # If the user provided an integer, go up as many times as asked
+  local i
   dest="./"
   if [[ "$1" = <-> ]]
   then
@@ -53,12 +49,6 @@ bd () {
 }
 _bd () {
   # Get parents (in reverse order)
-  local num_folders_we_are_in=${#${(ps:/:)${PWD}}}
-  local i
-  for i in {$((num_folders_we_are_in+1))..2}
-  do
-    reply=($reply "`echo $PWD | cut -d'/' -f$i`")
-  done
-  reply=($reply "/")
+  reply=(${(Oa)${(ps:/:)${PWD}}[1,-2]} "/")
 }
 compctl -V directories -K _bd bd
